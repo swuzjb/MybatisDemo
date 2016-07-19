ed79f2f500ada4eda75e95aded10e9d9a7e7b596
zhanjingbo <zhanjingbo@172.31.99.152>
2016-07-19 21:37:57 +0800

    添加一对一映射部分

---
+++ src/site/zhanjingbo/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package site.zhanjingbo.mapper;
+
+import site.zhanjingbo.model.Role;
+
+public interface RoleMapper {
+	public Role getRoleById(Integer id);
+}
--- src/site/zhanjingbo/model/User.java
+++ src/site/zhanjingbo/model/User.java
@@ -4,6 +4,12 @@ public class User {
 	private Integer id;
 	private String username;
 	private String password;
+	private Role role;
+	
+	
+	public User(){
+		super();
+	}
 	
 	public User(String username,String password){
 		super();
@@ -11,30 +17,43 @@ public class User {
 		this.password = password;
 	}
 	
-	
-	
 	public User(Integer id, String username, String password) {
 		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 	}
+	
 
+	public User(Integer id, String username, String password, Role role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
 
+	
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
 	}
 
-
-
-	public int getId() {
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
